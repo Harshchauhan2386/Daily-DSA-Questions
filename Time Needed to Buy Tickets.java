16b0ddd254e5ae9fb5d5170d0394654bc5e91453
Harshchauhan2386 <145603811+Harshchauhan2386@users.noreply.github.com>
2024-07-02 10:46:47 +0530

    Create Time Needed to Buy Tickets.java

---
+++ Time Needed to Buy Tickets.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int timeRequiredToBuy(int[] tickets, int k) {
+        int time = 0; 
+        while (true) {
+            for (int i = 0; i < tickets.length; i++) {
+                if (tickets[i] > 0) { 
+                    tickets[i] -= 1;  
+                    time++;  
+                }
+                if (i == k && tickets[i] == 0)
+                    return time; 
+            }
+        }
+    }
+}
