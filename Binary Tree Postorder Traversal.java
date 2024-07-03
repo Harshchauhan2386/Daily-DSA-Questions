565e9f0d480c9238bbc67c2d3800bd1676e3a238
Harshchauhan2386 <145603811+Harshchauhan2386@users.noreply.github.com>
2024-07-03 11:56:30 +0530

    Create Binary Tree Postorder Traversal.java

---
+++ Binary Tree Postorder Traversal.java
@@ -0,0 +1,64 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+ /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public void postorder(TreeNode root, ArrayList<Integer> array)
+    {
+        if(root==null)
+        {
+            return;
+        }
+        
+        postorder(root.left,array);
+        postorder(root.right,array);
+        array.add(root.val);
+    }
+    public List<Integer> postorderTraversal(TreeNode root) {
+        ArrayList<Integer> array =new ArrayList<Integer>();
+        postorder(root,array);
+        return array;
+    }
+}
